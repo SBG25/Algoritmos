a10d366ff03dd03c0d18da597279dceedca09ba6
Mendibj <37113509+Mendibj@users.noreply.github.com>
2019-05-09 12:15:17 +0200

    Create ExecutionTime.java

---
+++ src/executiontime/ExecutionTime.java
@@ -0,0 +1,18 @@
+package executiontime;
+
+public class ExecutionTime {
+	private static long startTime;
+	
+	private ExecutionTime() {
+		
+	}
+
+	public static void startTime() {
+		startTime = System.nanoTime();
+	}
+	
+	public static void endTime() {
+		long endTime = System.nanoTime();
+		System.out.println((endTime - startTime)/1000000);
+	}
+}
